cb3d1104a287eb132b5f1655f459ccfe9f485bb8
damon.wu <damon.wu@myob.com>
2020-02-18 08:29:13 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import fetch from 'node-fetch';
+
+const fetchData = () => fetch('https://jsonplaceholder.typicode.com/users');
+
+export const fetchDataWrapper = async (setData) => {
+  try {
+    const res = await fetchData();
+    const data = await res.json();
+    console.log(data);
+    setData(data);
+    return data;
+  } catch(e) {
+    console.log("Error on: ", e);
+    return e;
+  }
+}
 
 function App() {
+  const [theData, setData] = useState([]);
+  useEffect(() => {
+    fetchDataWrapper(setData);
+  }, []);
+
+  console.log('now: ', theData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test on Mock API !!! by using jest mock</p>
+      {theData? theData.map(data => <p key={data.name}>{data.name}</p>) : <p>Not yet..</p>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import * as fetch from "node-fetch";
+import { fetchDataWrapper } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test APP", () => {
+  it('test fetchDataWrapper', async (done) => {
+    const spy = jest.spyOn(fetch, 'default');
+    spy.mockImplementation(
+      () => (
+        new fetch.Response(
+        JSON.stringify({data: 1}),
+          {
+            status: 200,
+          }
+        ))
+    );
+    try{
+      const result = await fetchDataWrapper(jest.fn());
+      if(result){
+        console.log(result);
+        expect(result.data).toEqual(1);
+      }else {
+        done.fail('Expected to throw an Exception');
+      }
+    }catch(e) {
+      console.log("Error is: ", e);
+    }
+    done();
+  }, 1000);
 });
